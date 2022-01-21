42698f364e76e3ca11c5f6872e2c0575cd9bfbb6
Eduardo <baez.e7@gmail.com>
2022-01-20 21:51:17 -0300

    first commit

---
+++ PaymentApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PaymentApp
+//
+//  Created by Eduardo Báez on 20-01-22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
